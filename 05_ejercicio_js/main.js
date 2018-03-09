f7d2037edc9ecdf9d4a1ae9c8525b3966b3745f3
Marina Rico <maarinarico@gmail.com>
2018-03-09 16:35:18 +0100

    Acabo ejercicios jquery

--- 05_ejercicio_js/main.js
+++ 05_ejercicio_js/main.js
@@ -1,19 +1,59 @@
-/*$("#carrete").on(
+$("index.html").ready(function(){
+    $("h1").show()
+    $("#carrete").hide()
+});
 
-function(){ $("#carrete").hide()
+$("h1").on("click", function(){
+    $("#carrete").fadeIn(3000)
+    $("h1").hide()
+    $("#carrete").show()
+});
 
+$("#1").on("mouseover", function(){
+    $("#1").css("width", "300")
+    $("#1").css("transform", "rotate(360deg)")
+    $("#1").css("transform", "0.5s")
+});
+
+$("#1").on("mouseover", function(){
+    $("#1").css("width", "250")
+    $("#1").css("transform", "rotate(360deg)")
+    $("#1").css("transform", "0.5s")
 });
 
 
-$(document).on("click.mouseover",
+$("#2").on("mouseover", function(){
+    $("#2").css("width", "300")
+    $("#2").css("transform", "rotate(360deg)")
+    $("#2").css("transform", "0.5s")
+});
 
-function(){ $("#carrete").show()
+$("#2").on("mouseover", function(){
+    $("#2").css("width", "250")
+    $("#2").css("transform", "rotate(360deg)")
+    $("#2").css("transform", "0.5s")
+});
 
-}); */
+$("#3").on("mouseover", function(){
+    $("#3").css("width", "300")
+    $("#3").css("transform", "rotate(360deg)")
+    $("#3").css("transform", "0.5s")
+});
 
+$("#3").on("mouseover", function(){
+    $("#3").css("width", "250")
+    $("#3").css("transform", "rotate(360deg)")
+    $("#3").css("transform", "0.5s")
+});
 
-$("h1").on("click.mouseover",
+$("#4").on("mouseover", function(){
+    $("#4").css("width", "300")
+    $("#4").css("transform", "rotate(360deg)")
+    $("#4").css("transform", "0.5s")
+});
 
-function(){ $("img").replaceWith("src", "#carrete")
-
+$("#4").on("mouseover", function(){
+    $("#4").css("width", "250")
+    $("#4").css("transform", "rotate(360deg)")
+    $("#4").css("transform", "0.5s")
 });
